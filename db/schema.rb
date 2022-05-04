2f5c207cd4b7000eadfb48943be18646f06f350b
kizunas <8s6s7e@gmail.com>
2022-05-05 04:14:21 +0900

    comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_24_195713) do
+ActiveRecord::Schema.define(version: 2022_05_04_185405) do
+
+  create_table "comments", charset: "utf8mb4", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.text "content"
